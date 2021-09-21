8963c500d3b5f703c992ce1c6da3cd76cdb21403
johanla0 <>
2021-09-22 00:18:52 +0300

    Replace pica with image-blob-reduce, update form

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 const API_ENDPOINT = '';
 
 const axios = require('axios').default;
-const pica = require('pica')();
+// const pica = require('pica')();
+const reduce = require('image-blob-reduce')();
+
+const addFileButton = document.getElementById('add-button');
+
+addFileButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const newFileInput = document.createElement('input');
+  newFileInput.setAttribute('type', 'file');
+  newFileInput.setAttribute('name', 'file');
+  newFileInput.setAttribute('accept', 'image/jpg,image/jpeg');
+  newFileInput.classList.add('form-control', 'files', 'mb-3');
+  e.target
+    .closest('div')
+    .prepend(newFileInput);
+});
 
 const form = document.getElementById('send-paperwork-form');
 
@@ -14,27 +29,27 @@ form.addEventListener('submit', (e) => {
 
   const formData = new FormData(form);
   const recipient = formData.get('recipient');
-  const files = formData.getAll('files');
+  const files = formData.getAll('file');
   const filesToSend = [];
-  files.map((from) => {
-    pica
-      .resize(from, to)
-      .then((result) => pica.toBlob(result, 'image/jpeg', 0.9))
-      .then((blob) => filesToSend.push(blob));
-  });
+  // files.map((from) => {
+  //   pica
+  //     .resize(from, to)
+  //     .then((result) => pica.toBlob(result, 'image/jpeg', 0.9))
+  //     .then((blob) => filesToSend.push(blob));
+  // });
   // console.log(filesToSend.length);
   const formToSend = new FormData();
   formToSend.append('recipient', `test${recipient}`);
   filesToSend.map((file) => {
     formToSend.append(file.name, file);
   });
-  axios({
-    method: 'post',
-    url: API_ENDPOINT,
-    data: formToSend,
-    headers: { 'Content-Type': 'multipart/form-data' },
-  })
-    .then()
-    // eslint-disable-next-line no-console
-    .catch((err) => console.error(err));
+  // axios({
+  //   method: 'post',
+  //   url: API_ENDPOINT,
+  //   data: formToSend,
+  //   headers: { 'Content-Type': 'multipart/form-data' },
+  // })
+  //   .then()
+  //   // eslint-disable-next-line no-console
+  //   .catch((err) => console.error(err));
 });
