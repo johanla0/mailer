db5ca066ba3c927e7e52f31b112fdb374aa46d54
johanla0 <>
2021-09-23 23:54:07 +0300

    Resize files on input change

--- src/index.js
+++ src/index.js
@@ -5,44 +5,56 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 const API_ENDPOINT = '';
 
 const axios = require('axios').default;
-// const pica = require('pica')();
 const reduce = require('image-blob-reduce')();
 
-const addFileButton = document.getElementById('add-button');
+const resizedFiles = [];
 
+const resizeFile = (file) => reduce.toBlob(file, {
+  max: 1280,
+  unsharpAmount: 80,
+  unsharpRadius: 0.6,
+  unsharpThreshold: 2,
+});
+
+const onchangeCallback = async (e) => {
+  const result = await resizeFile(e.target.files[0]);
+  resizedFiles.push({
+    name: e.target.files[0].name,
+    blob: result,
+  });
+};
+
+const inputFile = document.getElementById('file');
+inputFile.addEventListener('change', onchangeCallback);
+
+const addFileButton = document.getElementById('add-button');
 addFileButton.addEventListener('click', (e) => {
   e.preventDefault();
   const newFileInput = document.createElement('input');
   newFileInput.setAttribute('type', 'file');
   newFileInput.setAttribute('name', 'file');
   newFileInput.setAttribute('accept', 'image/jpg,image/jpeg');
-  newFileInput.classList.add('form-control', 'files', 'mb-3');
-  e.target
-    .closest('div')
-    .prepend(newFileInput);
+  newFileInput.classList.add('form-control', 'files', 'my-3');
+  newFileInput.addEventListener('change', onchangeCallback);
+  e.target.parentNode.querySelector('div').append(newFileInput);
 });
 
-const form = document.getElementById('send-paperwork-form');
-
+const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const formData = new FormData(form);
   const recipient = formData.get('recipient');
+  const sender = formData.get('name');
+  const group = formData.get('group');
   const files = formData.getAll('file');
-  const filesToSend = [];
-  // files.map((from) => {
-  //   pica
-  //     .resize(from, to)
-  //     .then((result) => pica.toBlob(result, 'image/jpeg', 0.9))
-  //     .then((blob) => filesToSend.push(blob));
-  // });
-  // console.log(filesToSend.length);
+  const filenames = files.map((file) => file.name);
+  const filteredResizedFiles = resizedFiles.filter((file) => filenames.includes(file.name));
   const formToSend = new FormData();
   formToSend.append('recipient', `test${recipient}`);
-  filesToSend.map((file) => {
-    formToSend.append(file.name, file);
-  });
+  formToSend.append('sender', sender);
+  formToSend.append('group', group);
+  filteredResizedFiles.map((file) => formToSend.append('file[]', file.blob, file.name));
   // axios({
   //   method: 'post',
   //   url: API_ENDPOINT,
