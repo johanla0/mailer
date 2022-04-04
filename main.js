3c13d2830498a5742ceea929e86a4a85d0f17b91
johanla0 <59951682+johanla0@users.noreply.github.com>
2022-04-04 10:47:16 +0000

    Deploying to gh-pages from @ johanla0/mailer@6b55c3a2b3510f9f454c0e7ebb048b182d14ad05 🚀

--- main.js
+++ main.js
@@ -2914,6 +2914,13 @@ const generatePreview = (file) => reduce.toBlob(file, {
           name: lodash_uniqueid__WEBPACK_IMPORTED_MODULE_1___default()(`${filename}_`),
           data: file,
         }));
+        const images = data.map((file) => `<p><img src=${file}></p>`);
+        const body = `<html>
+          <h2>Студент: ${studentName}</h2>
+          <h3>Группа: ${group}</h3>
+          <p>Кол-во файлов: <i>${data.length}</i></p>
+          ${images.join('')}
+          </html>`;
         // eslint-disable-next-line no-undef
         Email.send({
           SecureToken: '9391d7b5-e3c4-4a0d-92dc-2f28dfbf30dc',
@@ -2921,7 +2928,7 @@ const generatePreview = (file) => reduce.toBlob(file, {
           To: 'dolgov.is@irlc.msu.ru',
           From: 'dolgov.is@irlc.msu.ru',
           Subject: `${date}: test${recipientId} - ${studentName}`,
-          Body: `Ответ студента ${studentName}, группа: ${group}. Кол-во файлов: ${data.length}`,
+          Body: body,
           Attachments: attachments,
         }).then((response) => {
           headerSuccess.classList.remove('d-none');
